4da640908d21eca1c044e5b24abd57f1f61af04b
ndalilavin <vinson.steve@student.moringaschool.com>
2022-10-13 00:04:22 +0300

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_30_083301) do
+ActiveRecord::Schema.define(version: 2022_10_11_123017) do
 
   create_table "plants", force: :cascade do |t|
     t.string "name"
     t.string "image"
-    t.float "price"
+    t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
